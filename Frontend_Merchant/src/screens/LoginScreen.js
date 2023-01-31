2dd396b6b70ea4132a964bf91a25e4dc0b72cbcd
Panat Siriwongtrakool <s6201012610052@email.kmutnb.ac.th>
2023-01-31 21:14:35 +0700

    Refactor Login Screen

--- Frontend_Merchant/src/screens/LoginScreen.js
+++ Frontend_Merchant/src/screens/LoginScreen.js
@@ -1,47 +1,57 @@
-import { View, Text, Image, SafeAreaView, ImageBackground } from "react-native";
+import {
+    View,
+    Text,
+    Image,
+    SafeAreaView,
+    ImageBackground,
+    StyleSheet,
+} from "react-native";
 import React from "react";
 import Btn_GoogleLogin from "../components/Btn_GoogleLogin";
 
 const LoginScreen = () => {
     const image = require("../assets/images/backgrounds/Login.png");
-    // const image = { uri: "https://reactjs.org/logo-og.png" };
     const loginWithGoogle = () => {
         console.log("Button pressed");
     };
     return (
-        <SafeAreaView
-            style={{
-                flex: 1,
-                // justifyContent: "center",
-                // margin: 20,
-                backgroundColor: "#F5F5F5",
-            }}
-        >
+        <SafeAreaView style={styles.container}>
             <ImageBackground
                 source={image}
                 resizeMode="cover"
-                style={{ width: "100%", height: "100%" }}
+                style={styles.image}
             >
                 <View style={{ alignItems: "center" }}>
                     <Image
                         source={require("../assets/icons/LangMor.png")}
-                        style={{ width: 270, height: 251.3 }}
+                        style={{ width: 280, height: 250 }}
                     />
                 </View>
-                <Text
-                    style={{
-                        marginTop: 20,
-                        marginBottom: 20,
-                        fontSize: 30,
-                        fontFamily: "Kanit-Bold",
-                    }}
-                >
-                    Login{"\n"}Your Account
-                </Text>
-                <Btn_GoogleLogin onPress={loginWithGoogle} />
+                <Text style={styles.text}>Login{"\n"}Your Account</Text>
+                <View style={{ margin: 20 }}>
+                    <Btn_GoogleLogin onPress={loginWithGoogle} />
+                </View>
             </ImageBackground>
         </SafeAreaView>
     );
 };
 
 export default LoginScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#F5F5F5",
+    },
+    image: {
+        flex: 1,
+        justifyContent: "center",
+    },
+    text: {
+        marginTop: 20,
+        marginBottom: 20,
+        marginLeft: 20,
+        fontSize: 30,
+        fontFamily: "Kanit-Bold",
+    },
+});
