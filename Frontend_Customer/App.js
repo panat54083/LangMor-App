59885012814931c974e80aa392625aac570d3721
Pakpoom-Phoonpunt <s6201012620210@email.kmutnb.ac.th>
2023-01-31 19:23:53 +0700

    crate build dev and basic googleLogin

--- Frontend_Customer/App.js
+++ Frontend_Customer/App.js
@@ -1,12 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { ANDROID_CLIENT_ID, IOS_CLIENT_ID, EXPO_CLIENT_ID } from "@env";
+import GoogleLogin from './src/screens/GoogleLogin';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>{ANDROID_CLIENT_ID}!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GoogleLogin/>
   );
 }
 
