3696e993640f8b7dfcad5b3c38892bab37e4b496
Pakpoom-Phoonpunt <s6201012620210@email.kmutnb.ac.th>
2023-02-03 15:15:15 +0700

    modify header in MarketlistScreen

--- Frontend_Customer/src/screens/MarketList.js
+++ Frontend_Customer/src/screens/MarketList.js
@@ -1,8 +1,23 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useEffect } from "react";
 import Searchbar from "../components/searchs/Searchbar";
+import Fav from "../components/buttons/Fav";
 const MarketList = ({ navigation }) => {
     //ของจริงใช้ fetch ข้อมูลจาก backend
+    useEffect(() => {
+        navigation.setOptions({
+            title: "สั่งอาหาร",
+            headerTitleAlign: "center",
+            headerTitleStyle: styles.headerTitle,
+            headerStyle: styles.headerStyle,
+            headerRight: () => (
+                <View style={{ marginRight: "20%" }}>
+                    <Fav />
+                </View>
+            ),
+        });
+    }, []);
+
     const exampleData = [
         {
             id: 1,
@@ -53,15 +68,31 @@ const MarketList = ({ navigation }) => {
             owner: "testuser01@gmail.com",
         },
     ];
-    
+    let allTags = ["ทั้งหมด", "ก๋วยเตี๋ยว", "เกาเหลา", "ตามสั่ง", "ของหวาน"];
+
     return (
         <View>
-            <Searchbar/>
-            <Text>Hello MarketList</Text>
+            <Searchbar />
         </View>
     );
 };
 
 export default MarketList;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    headerTitle: {
+        color: "white",
+        fontFamily: "Kanit-Bold",
+        fontSize: 28,
+    },
+    headerStyle: {
+        backgroundColor: "#FF7A00",
+        borderWidth: 1,
+        borderRadius: 5,
+        borderColor: "#FF7A00",
+        shadowColor: "#000000",
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.9,
+        shadowRadius: 10,
+    },
+});
