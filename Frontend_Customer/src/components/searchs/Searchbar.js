7d04942eddcf8de7adf24dfc336d287e4e66e36c
Pakpoom-Phoonpunt <s6201012620210@email.kmutnb.ac.th>
2023-02-03 14:59:45 +0700

    add seachbar component

---
+++ Frontend_Customer/src/components/searchs/Searchbar.js
@@ -0,0 +1,43 @@
+import { StyleSheet, Text, TextInput, View } from "react-native";
+import React, { useState } from "react";
+import Ionicons from "@expo/vector-icons/Ionicons";
+
+const Searchbar = () => {
+    const [text, setText] = useState("");
+
+    return (
+        <View style={styles.inputContainer}>
+            <Ionicons name="search" size={20} style={styles.icon}/>
+            <TextInput
+                style={styles.input}
+                placeholder="Search"
+                onChangeText={(text) => setText(text)}
+                value={text}
+            />
+        </View>
+    );
+};
+
+export default Searchbar;
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+        backgroundColor: "white",
+        borderRadius: 25,
+        padding: 15,
+        margin: 10,
+        width:275,
+        height:55
+    },
+    icon: {
+        width: 20,
+        height: 20,
+        marginRight: 10,
+    },
+    input: {
+        flex:1,
+        fontSize: 16
+      },
+});
--- Frontend_Customer/src/screens/MarketList.js
+++ Frontend_Customer/src/screens/MarketList.js
@@ -1,15 +1,67 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import Searchbar from "../components/searchs/Searchbar";
+const MarketList = ({ navigation }) => {
+    //ของจริงใช้ fetch ข้อมูลจาก backend
+    const exampleData = [
+        {
+            id: 1,
+            restaurantName: "ร้านป้านิด",
+            description: "ร้านป้านิดสุดอร่อย",
+            tags: ["ตามสั่ง"],
+            address: "address01",
+            owner: "testuser01@gmail.com",
+        },
+        {
+            id: 2,
+            restaurantName: "ร้านป้านิด",
+            description: "ร้านป้านิดสุดอร่อย",
+            tags: ["ตามสั่ง"],
+            address: "address01",
+            owner: "testuser01@gmail.com",
+        },
+        {
+            id: 3,
+            restaurantName: "ร้านป้านิด",
+            description: "ร้านป้านิดสุดอร่อย",
+            tags: ["ตามสั่ง"],
+            address: "address01",
+            owner: "testuser01@gmail.com",
+        },
+        {
+            id: 4,
+            restaurantName: "ร้านป้านิด",
+            description: "ร้านป้านิดสุดอร่อย",
+            tags: ["ตามสั่ง"],
+            address: "address01",
+            owner: "testuser01@gmail.com",
+        },
+        {
+            id: 5,
+            restaurantName: "ร้านป้านิด",
+            description: "ร้านป้านิดสุดอร่อย",
+            tags: ["ตามสั่ง"],
+            address: "address01",
+            owner: "testuser01@gmail.com",
+        },
+        {
+            id: 6,
+            restaurantName: "ร้านป้านิด",
+            description: "ร้านป้านิดสุดอร่อย",
+            tags: ["ตามสั่ง"],
+            address: "address01",
+            owner: "testuser01@gmail.com",
+        },
+    ];
+    
+    return (
+        <View>
+            <Searchbar/>
+            <Text>Hello MarketList</Text>
+        </View>
+    );
+};
 
-const MarketList = () => {
-  const exampleData = [{}] //ของจริงใช้ fetch ข้อมูลจาก backend
-  return (
-    <View>
-      <Text>Hello MarketList</Text>
-    </View>
-  )
-}
+export default MarketList;
 
-export default MarketList
-
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
