fd746cde05999a3fc4d38acb97dc11705e814e0e
Panat Siriwongtrakool <s6201012610052@email.kmutnb.ac.th>
2023-02-07 22:21:35 +0700

    Initial Top Tab navigaiton in Menu

--- Frontend_Merchant/src/config/Tabs/MenuTabs.js
+++ Frontend_Merchant/src/config/Tabs/MenuTabs.js
@@ -1,19 +1,60 @@
 //Packages
-import React from "react";
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import React, { useEffect } from "react";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 //Components
 import { StyleSheet, Text, View } from "react-native";
 //Screens
 import MenuManage from "../../screens/Menu/MenuManage";
-import OptionsManage from "../../screens/Orders/OptionsManage";
+import OptionsManage from "../../screens/Menu/OptionsManage";
+import BackScreen from "../../components/buttons/BackScreen";
 //Configs
 const Tab = createMaterialTopTabNavigator();
 
-const MenuTabs = () => {
+const MenuTabs = ({ navigation }) => {
+    useEffect(() => {
+        navigation.setOptions({
+            title: "จัดการเมนูร้านค้า",
+            headerTitleStyle: {
+                fontFamily: "Kanit-Bold",
+                fontSize: 22,
+            },
+            headerLeft: () => (
+                <BackScreen
+                    onPress={() => navigation.goBack()}
+                    color="#FF7A00"
+                />
+            ),
+        });
+    }, []);
+
     return (
-        <Tab.Navigator>
-            <Tab.Screen name="MenuManage" component={MenuManage} />
-            <Tab.Screen name="OptionsManage" component={OptionsManage} />
+        <Tab.Navigator
+            Style={{}}
+            screenOptions={{
+                tabBarActiveTintColor: "#ff4200",
+                tabBarInactiveTintColor: "#000",
+                tabBarLabelStyle: { fontSize: 16, fontFamily: "Kanit-Medium" },
+                tabBarStyle: {
+                    backgroundColor: "#FFE8E0",
+                    // backgroundColor: "#FFf",
+                },
+                tabBarIndicatorStyle: {
+                    borderBottomColor: "#FF4200",
+                    borderBottomWidth: 4,
+                    // color: "black"
+                },
+            }}
+        >
+            <Tab.Screen
+                name="MenuManage"
+                component={MenuManage}
+                options={{ tabBarLabel: "เมนูร้าน" }}
+            />
+            <Tab.Screen
+                name="OptionsManage"
+                component={OptionsManage}
+                options={{ tabBarLabel: "ตัวเลือกเสริม" }}
+            />
         </Tab.Navigator>
     );
 };
