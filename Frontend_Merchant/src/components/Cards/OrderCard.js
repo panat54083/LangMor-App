bb620fabc803aa70d5d7022ed4599fd4f95841b7
Panat Siriwongtrakool <s6201012610052@email.kmutnb.ac.th>
2023-02-13 23:59:02 +0700

    Add initial OrderCard component

---
+++ Frontend_Merchant/src/components/Cards/OrderCard.js
@@ -0,0 +1,80 @@
+//Packages
+import React from "react";
+//Components
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { Entypo } from "@expo/vector-icons";
+
+const OrderCard = () => {
+    return (
+        <TouchableOpacity style={styles.container}>
+            <View style={[styles.id]}>
+                <View style={styles.box_id}>
+                    <Text style={[styles.font_id]}>
+                        ID {"\n"}
+                        <Text style={{ color: "white" }}>0001</Text>
+                    </Text>
+                </View>
+            </View>
+            <View style={[styles.name, { justifyContent: "space-around" }]}>
+                <Text style={styles.font_name}>ปณัฎฐ์</Text>
+                <View style={{ flexDirection: "row" }}>
+                    <Entypo name="back-in-time" size={24} color="#FF4200" />
+                    <Text
+                        style={[
+                            styles.font_name,
+                            { color: "#FF4200", marginLeft: 4 },
+                        ]}
+                    >
+                        รับภายใน 10 นาที
+                    </Text>
+                </View>
+            </View>
+            <View style={styles.price}>
+                <Text style={styles.font_price}>120</Text>
+            </View>
+        </TouchableOpacity>
+    );
+};
+
+export default OrderCard;
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "white",
+        flexDirection: "row",
+        borderRadius: 15,
+    },
+    id: {
+        flex: 1,
+        margin: 10,
+    },
+    box_id: {
+        backgroundColor: "#FF7A00",
+        borderRadius: 10,
+        width: "100%",
+        justifyContent: "center",
+    },
+    font_id: {
+        fontFamily: "Kanit-Bold",
+        fontSize: 25,
+        paddingHorizontal: 5,
+    },
+    name: {
+        flex: 2,
+        // paddingTop: 5,
+    },
+    font_name: {
+        fontFamily: "Kanit-Bold",
+        fontSize: 19,
+    },
+    font_price: {
+        fontFamily: "Kanit-Bold",
+        fontSize: 19,
+    },
+    price: {
+        flex: 1,
+        paddingTop: 5,
+        alignItems: "flex-end",
+        right: 10,
+    },
+});
--- Frontend_Merchant/src/screens/Orders/OrderManage.js
+++ Frontend_Merchant/src/screens/Orders/OrderManage.js
@@ -3,6 +3,7 @@ import React, { useEffect } from "react";
 //Components
 import { StyleSheet, Text, View } from "react-native";
 import BackScreen from "../../components/buttons/BackScreen";
+import OrderCard from "../../components/Cards/OrderCard";
 
 const OrderManage = ({ navigation }) => {
     useEffect(() => {
@@ -22,12 +23,17 @@ const OrderManage = ({ navigation }) => {
     }, []);
 
     return (
-        <View>
-            <Text>OrderManage</Text>
+        <View style={styles.container}>
+            <OrderCard/>
         </View>
     );
 };
 
 export default OrderManage;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        padding:20,
+    }
+});
