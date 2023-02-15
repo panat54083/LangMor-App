5f1592aab070cf6799293ab32366c4e6d7b8a70b
Panat Siriwongtrakool <s6201012610052@email.kmutnb.ac.th>
2023-02-16 00:48:47 +0700

    Initail message component

--- Frontend_Customer/src/screens/Chat/Chat.js
+++ Frontend_Customer/src/screens/Chat/Chat.js
@@ -1,9 +1,17 @@
 //packages
 import React, { useContext, useEffect } from "react";
 //components
-import { ScrollView, StyleSheet, Text, View, Button } from "react-native";
+import {
+    ScrollView,
+    StyleSheet,
+    Text,
+    View,
+    Button,
+    FlatList,
+} from "react-native";
 import BackScreen from "../../components/buttons/BackScreen";
-import ChatInput from "../../components/cards/ChatInput";
+import ChatInput from "../../components/cards/Chat/ChatInput";
+import MessageModel from "../../components/cards/Chat/MessageModel";
 //configs
 import BasketContext from "../../hooks/context/BasketContext";
 import UserContext from "../../hooks/context/UserContext";
@@ -13,7 +21,7 @@ const Chat = ({ navigation }) => {
     const { state } = useContext(UserContext);
     useEffect(() => {
         navigation.setOptions({
-            title: "เพิ่มเมนูอาหาร",
+            title: "หน้าแชท",
             headerTitleStyle: {
                 fontFamily: "Kanit-Bold",
                 fontSize: 22,
@@ -26,7 +34,6 @@ const Chat = ({ navigation }) => {
             ),
         });
     }, []);
-
     const old_restaurantData = {
         address: "address01",
         description: "ร้านป้านิดสุดอร่อย",
@@ -36,8 +43,7 @@ const Chat = ({ navigation }) => {
         restaurantName: "ร้านป้านิด",
         tags: ["ตามสั่ง"],
     };
-
-    const new_restaurantData = {
+    const demo_restaurantData = {
         __v: 4,
         _id: "63e188c58ae333a7867b14f2",
         address: "หน้าวัดน้อย",
@@ -46,28 +52,46 @@ const Chat = ({ navigation }) => {
         name: "ร้านอาหารแอบแซ่บ",
         owner: "63e1887c8ae333a7867b14ef",
         phone: "0984396379",
-        picture: {
-            assetId: "48",
-            duration: null,
-            exif: null,
-            height: 637,
-            rotation: null,
-            type: "image",
-            uri: "file:///data/user/0/com.kmutnb.frontendlangmormerchant/cache/ImagePicker/321d95c5-2270-4aed-a895-1ec658c09e93.jpeg",
-            width: 481,
-        },
+        picture: null,
         types: ["อาหารคาว", "เครื่องดื่ม", "ขนมหวาน", "ของว่าง", "อาหารคลีน"],
         updatedAt: "2023-02-13T08:19:27.160Z",
         worker: [],
+        description: "",
     };
-
+    const listMessages = [
+        { user: "A", message: "Hello B", id: 1 },
+        { user: "Dipper Pine", message: "Hello A", id: 2 },
+        { user: "Dipper Pine", message: "Hello A", id: 2 },
+        { user: "Dipper Pine", message: "Hello A", id: 2 },
+        { user: "Dipper Pine", message: "Hello A", id: 2 },
+        { user: "Dipper Pine", message: "Hello A", id: 2 },
+        { user: "Dipper Pine", message: "Hello A", id: 2 },
+        { user: "Dipper Pine", message: "Hello A", id: 2 },
+        { user: "Dipper Pine", message: "Hello A", id: 2 },
+        { user: "Dipper Pine", message: "Hello A", id: 2 },
+    ];
     const handleGetInfo = () => {
-        console.log(basketDetail.restaurant);
+        connsole.log(basketDetail.foods[0].options);
     };
     return (
         <View style={styles.main_container}>
             {/* <Button onPress={handleGetInfo} title="Press me!!" /> */}
-            <View style={styles.messages_container}></View>
+            <View style={styles.messages_container}>
+                {listMessages[0] ? (
+                    <FlatList
+                        data={listMessages}
+                        renderItem={({ item }) => (
+                            <MessageModel
+                                message={item}
+                                user={state.userData.name}
+                            />
+                        )}
+                        keyExtractor={(item, index) => index}
+                    />
+                ) : (
+                    <Text>No message</Text>
+                )}
+            </View>
             <ChatInput />
         </View>
     );
@@ -81,6 +105,5 @@ const styles = StyleSheet.create({
     },
     messages_container: {
         flex: 10,
-        // backgroundColor: "red",
     },
 });
