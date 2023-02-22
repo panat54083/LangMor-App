0e47bc30e166b264b13fc773289a1cb895dd5f3e
Panat Siriwongtrakool <s6201012610052@email.kmutnb.ac.th>
2023-02-22 13:56:52 +0700

    Initial Component in ShowOrder Screen

--- Frontend_Merchant/src/screens/Orders/ShowOrder.js
+++ Frontend_Merchant/src/screens/Orders/ShowOrder.js
@@ -1,17 +1,48 @@
 //Packages
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 //Components
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button } from "react-native";
+import AddressBoxDetail from "../../components/Cards/Order/AddressBoxDetail";
+import BackScreen from "../../components/buttons/BackScreen";
+import OrderSummary from "../../components/Cards/Order/OrderSummary";
+//Configs
+import { IP_ADDRESS } from "@env";
 
-const ShowOrder = ({navigation, route}) => {
-    const room = route.params
+const ShowOrder = ({ navigation, route }) => {
+    const room = route.params;
+
+    useEffect(() => {
+        navigation.setOptions({
+            title: `ออเดอร์ของ ${room.customer.name}`,
+            headerTitleStyle: {
+                fontFamily: "Kanit-Bold",
+                fontSize: 20,
+            },
+            headerLeft: () => (
+                <BackScreen
+                    onPress={() => navigation.goBack()}
+                    color="#FF7A00"
+                />
+            ),
+        });
+    }, []);
+
+    const handleDebugger = () => {
+        console.log(room)
+    }
     return (
-        <View>
-            <Text>ShowOrder</Text>
+        <View style={styles.container}>
+            <Button title="Debugger" onPress={handleDebugger}/>
+            <AddressBoxDetail address={"eiei"}/>
+            <OrderSummary/>
         </View>
     );
 };
 
 export default ShowOrder;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+    },
+});
