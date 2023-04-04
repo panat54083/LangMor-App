33ac04dbb35efcea43089317c7ca72f7fdbf8f9a
Pakpoom-Phoonpunt <s6201012620210@email.kmutnb.ac.th>
2023-04-04 23:53:17 +0700

    fix search restaurant

--- Backend_LangMor/controllers/restaurantController.js
+++ Backend_LangMor/controllers/restaurantController.js
@@ -319,22 +319,27 @@ exports.restaurantSearch = async (req, res) => {
     const { keyword } = req.query;
     Restaurant.aggregate([
         {
-            $lookup: {
-                from: "foods",
-                localField: "_id",
-                foreignField: "restaurant_id",
-                as: "foods",
-            },
+          $lookup: {
+            from: "foods",
+            localField: "_id",
+            foreignField: "restaurant_id",
+            as: "foods",
+          },
         },
         {
-            $match: {
+          $match: {
+            $and: [
+              { closed: false },
+              {
                 $or: [
-                    { "foods.name": { $regex: `${keyword}`, $options: "i" } },
-                    { name: { $regex: `${keyword}`, $options: "i" } },
+                  { "foods.name": { $regex: `${keyword}`, $options: "i" } },
+                  { name: { $regex: `${keyword}`, $options: "i" } },
                 ],
-            },
+              },
+            ],
+          },
         },
-    ]).exec((err, resResults) => {
+      ]).exec((err, resResults) => {
         if (err) {
             // handle error
         } else {
